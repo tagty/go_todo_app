7577cc2b65458662dd1080282a404364dfe3420f
Tetsuya Taguchi <bouzu.ao@gmail.com>
2023-08-09 08:41:04 +0900

    end point #5

---
+++ entity/task.go
@@ -0,0 +1,21 @@
+package entity
+
+import "time"
+
+type TaskID int64
+type TaskStatus string
+
+const (
+	TaskStatusTodo  TaskStatus = "todo"
+	TaskStatusDoing TaskStatus = "doing"
+	TaskStatusDone  TaskStatus = "done"
+)
+
+type Task struct {
+	ID      TaskID     `json:"id" db:"id"`
+	Title   string     `json:"title" db:"title"`
+	Status  TaskStatus `json:"status" db:"status"`
+	Created time.Time  `json:"created" db:"created"`
+}
+
+type Tasks []*Task
